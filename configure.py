5c966d17e57731759c720bd9f9c30b1cf2cdb2ab
Mattias Jansson <mattias@rampantpixels.com>
2018-03-02 10:01:57 +0100

    additional platform cleanups

--- configure.py
+++ configure.py
@@ -33,7 +33,7 @@ if generator.is_subninja():
 includepaths = generator.test_includepaths()
 
 test_cases = ['alloc']
-if toolchain.is_monolithic() or target.is_ios() or target.is_android() or target.is_tizen() or target.is_pnacl():
+if toolchain.is_monolithic() or target.is_ios() or target.is_android() or target.is_tizen():
   #Build one fat binary with all test cases
   test_resources = []
   test_extrasources = []
